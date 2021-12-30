cb39ede0ac1819a2c6ef5a8b3ff2ada6caff30ea
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2021-12-30 09:57:01 +0900

    Create and style Feedback section

--- js/index.js
+++ js/index.js
@@ -11,3 +11,18 @@ for (var i = 0; i < listNav.length; i++) {
 
 // Counting up Number in Stats section
 $('.counter').countUp();
+
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+    }
+})
